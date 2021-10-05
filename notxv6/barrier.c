1adc3fd243e49efaaa518be8b2eb986e38c0c123
Fenghao Dong <dfh@pku.edu.cn>
2021-10-05 12:33:05 +0800

    Lab7: Multithreading

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -30,7 +30,29 @@ barrier()
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
-  
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  while (bstate.nthread != 0 && round != bstate.round)
+  {
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  }
+  bstate.nthread += 1;
+  while (bstate.nthread != nthread && round == bstate.round)
+  {
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  }
+  if (bstate.nthread == nthread)
+  {
+    bstate.round += 1;
+    pthread_cond_broadcast(&bstate.barrier_cond);
+  }
+  else if (bstate.nthread == 1)
+  {
+    round += 1;
+    pthread_cond_broadcast(&bstate.barrier_cond);
+  }
+  bstate.nthread -= 1;
+  pthread_mutex_unlock(&bstate.barrier_mutex);
+  return;
 }
 
 static void *
--- user/uthread.c
+++ user/uthread.c
@@ -12,9 +12,23 @@
 
 
 struct thread {
+  uint64     ra;
+  uint64     sp;
+  // callee-saved
+  uint64     s0;
+  uint64     s1;
+  uint64     s2;
+  uint64     s3;
+  uint64     s4;
+  uint64     s5;
+  uint64     s6;
+  uint64     s7;
+  uint64     s8;
+  uint64     s9;
+  uint64     s10;
+  uint64     s11;
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
-
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
@@ -55,7 +69,8 @@ thread_schedule(void)
     exit(-1);
   }
 
-  if (current_thread != next_thread) {         /* switch threads?  */
+  if (current_thread != next_thread) {
+    /* switch threads */
     next_thread->state = RUNNING;
     t = current_thread;
     current_thread = next_thread;
@@ -63,6 +78,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch((uint64)t,(uint64)next_thread);
   } else
     next_thread = 0;
 }
@@ -77,6 +93,9 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  t->sp=(uint64)(&t->state);
+  t->ra=(uint64)func;
+  return;
 }
 
 void 
